eb722159abd9c8fd06f57c5649b5737971ec3b25
jude <jude@affle.com>
2015-06-24 16:07:12 +0800

    initial setup for phonegap

--- socially/client/lib/app.js
+++ socially/client/lib/app.js
@@ -3,3 +3,13 @@
  */
 
 angular.module('socially', ['angular-meteor', 'ui.router']);
+
+
+function onReady() {
+    angular.bootstrap(document, ['socially']);
+}
+
+if (Meteor.isCordova)
+    angular.element(document).on("deviceready", onReady);
+else
+    angular.element(document).ready(onReady);
