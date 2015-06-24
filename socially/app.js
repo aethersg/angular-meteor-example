63954cdb6c72eb1098d0864469aa37a83af01f7a
jude <jude@affle.com>
2015-06-24 12:22:51 +0800

    3 way data binding with mongo DB example

--- socially/app.js
+++ socially/app.js
@@ -1,27 +1,36 @@
 /**
  * Created by judetan on 24/6/15.
  */
+
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient) {
 
     angular.module('socially', ['angular-meteor']);
 
-    angular.module("socially").controller("PartiesListCtrl", ['$scope',
-        function ($scope) {
+    angular.module("socially").controller("PartiesListCtrl", ['$scope','$meteor',
+        function ($scope,$meteor) {
 
-            $scope.parties = [
-                {
-                    'name': 'Dubstep-Free Zone',
-                    'description': 'Can we please just for an evening not listen to dubstep.'
-                },
-                {
-                    'name': 'All dubstep all the time',
-                    'description': 'Get it on!'
-                },
-                {
-                    'name': 'Savage lounging',
-                    'description': 'Leisure suit required. And only fiercest manners.'
-                }
-            ];
+            $scope.parties = $meteor.collection(Parties);
 
         }]);
+}
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        if (Parties.find().count() === 0) {
+
+            var parties = [
+                {'name': 'Dubstep-Free Zone',
+                    'description': 'Can we please just for an evening not listen to dubstep.'},
+                {'name': 'All dubstep all the time',
+                    'description': 'Get it on!'},
+                {'name': 'Savage lounging',
+                    'description': 'Leisure suit required. And only fiercest manners.'}
+            ];
+
+            for (var i = 0; i < parties.length; i++)
+                Parties.insert(parties[i]);
+
+        }
+    });
 }
