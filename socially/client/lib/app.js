e7936a3ad9877735934600495b83ecaff49f0610
jude <jude@affle.com>
2015-06-30 11:06:14 +0800

    removed bootstrap and added in angular-materials

--- socially/client/lib/app.js
+++ socially/client/lib/app.js
@@ -6,7 +6,8 @@ angular.module('socially', [
     'angular-meteor',
     'ui.router',
     'angularUtils.directives.dirPagination',
-    'uiGmapgoogle-maps'
+    'uiGmapgoogle-maps',
+    'ngMaterial'
 ]);
 
 
