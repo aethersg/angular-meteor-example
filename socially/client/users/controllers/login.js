91ec16a3a51949b21299e4569bde82fa1ac48cca
jude <jude@affle.com>
2015-06-30 12:25:00 +0800

    angular-material and custom Angular auth forms

---
+++ socially/client/users/controllers/login.js
@@ -0,0 +1,27 @@
+/**
+ * Created by judetan on 30/6/15.
+ */
+
+angular.module("socially").controller("LoginCtrl", ['$meteor', '$state',
+    function ($meteor, $state) {
+        var vm = this;
+
+        vm.credentials = {
+            email: '',
+            password: ''
+        };
+
+        vm.error = '';
+
+        vm.login = function () {
+            $meteor.loginWithPassword(vm.credentials.email, vm.credentials.password).then(
+                function () {
+                    $state.go('parties');
+                },
+                function (err) {
+                    vm.error = 'Login error - ' + err;
+                }
+            );
+        };
+    }
+]);
---
+++ socially/client/users/controllers/register.js
@@ -0,0 +1,26 @@
+/**
+ * Created by judetan on 30/6/15.
+ */
+angular.module("socially").controller("RegisterCtrl", ['$meteor', '$state',
+    function ($meteor, $state) {
+        var vm = this;
+
+        vm.credentials = {
+            email: '',
+            password: ''
+        };
+
+        vm.error = '';
+
+        vm.register = function () {
+            $meteor.createUser(vm.credentials).then(
+                function () {
+                    $state.go('parties');
+                },
+                function (err) {
+                    vm.error = 'Registration error - ' + err;
+                }
+            );
+        };
+    }
+]);
